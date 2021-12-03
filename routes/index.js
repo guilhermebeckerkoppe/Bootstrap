26c48e844232c5beb4bcc0abf06c865f07d90adb
guilhermebeckerkoppe <95240361+guilhermebeckerkoppe@users.noreply.github.com>
2021-12-03 11:01:08 -0300

    About

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'E-books' });
+  res.render('index', { title: 'E-books | Home' });
+});
+
+/* GET books page. */
+router.get('/books', function(req, res, next) {
+  res.render('books', { title: 'E-books | Books', h1 : 'Books' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'E-books | About', h1 : 'About Us' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'E-books | Contact', h1 : 'Contact' });
 });
 
 module.exports = router;
