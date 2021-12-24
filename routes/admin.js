d90b7c04b1cb0712f530943fa10310ebe332b0ce
guilhermebeckerkoppe <guilhermebeckerkoppe@gmail.com>
2021-12-24 16:47:09 -0300

    admin

--- routes/admin.js
+++ routes/admin.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('admin/index', { title: 'E-books | Admin', h1 : 'Admin' });
 });
 
+/* GET users listing. */
+router.get('/contacts', function(req, res, next) {
+  res.render('admin/contacts', { title: 'E-books | Contacts', h1 : 'Contacts' });
+});
+
 module.exports = router;
